6f8adaf6fabb5503ffd9a089be725d9dc433896d
Anthony Stone <ajs1@cam.ac.uk>
2019-02-06 12:41:49 +0000

    examples/diff.py: converted for python3

--- examples/diff.py
+++ examples/diff.py
@@ -46,19 +46,19 @@ with open(args.check) as CHK, open(args.new) as NEW:
       if re.match(r'Finished', chk) or re.match(r'Finished', new):
         if not re.match(r'Finished', chk):
           mismatches += 1
-          print "New file finished before check file"
+          print("New file finished before check file")
         elif not re.match(r'Finished', new):
-          print "Extra output in new file"
+          print("Extra output in new file")
         break
       new = re.sub(r'-(0.0+)\b', ' $1', new)
       chk = re.sub(r'-(0.0+)\b', ' $1', chk)
       if new != chk and not mismatches:
-        print "New output file {} differs from check file {}.".format(args.new,args.check)
+        print("New output file {} differs from check file {}.".format(args.new,args.check))
         mismatches += 1
-        print chk,
-        print new
+        print(chk, end=' ')
+        print(new)
   if mismatches:
-    print "{1d} lines failed to match"
+    print("{1d} lines failed to match")
   else:
-    print "New output file checked successfully"
+    print("New output file checked successfully")
 
