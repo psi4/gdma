6e5acce94de3c4b77b15d7690d862c60b27fa3d8
Lori A. Burns <lori.burns@gmail.com>
2023-05-22 23:44:45 -0400

    io patch, cmake, pybind

--- src/gdma.f90
+++ src/gdma.f90
@@ -1,4 +1,5 @@
-PROGRAM gdma
+module libgdma
+!program gdma
 
 !  Distributed Multipole Analysis for Gaussian Wavefunctions
 !
@@ -18,8 +19,13 @@ PROGRAM gdma
 !  along with this program; if not, write to
 !  the Free Software Foundation, Inc., 51 Franklin Street,
 !  Fifth Floor, Boston, MA 02110-1301, USA.
+!
+!  This version has been modified by Andy Simmonett (03/16) to link
+!  into Psi4, rather than serve as a standalone executable.
 
 USE input
+use iso_c_binding
+use iso_fortran_env, only : std_out=>output_unit
 USE version
 USE dma
 USE atom_grids, ONLY: debug_grid => debug
@@ -40,21 +46,74 @@ INTEGER :: ncoorb, maxl, cmax, nprim, nx, num, ich, mul
 INTEGER, ALLOCATABLE :: shell_type(:)
 INTEGER :: i, j, k, kp=0
 LOGICAL :: eof, fchk, first, ok=.false.
+INTEGER open_status, infile
 
 REAL(dp), ALLOCATABLE :: densty(:,:), dtri(:)
 INTEGER :: ir=5 ! Input stream
 
 LOGICAL :: verbose=.false., debug(0:2)=.false.
 
+CONTAINS
 
-print "(15x,a/15x,a//15x,a//15x,5a/14x,4a)",                           &
+
+subroutine run_gdma(c_outfilename, c_datfilename) bind(c, name='run_gdma')
+!character(kind=c_char,len=1), intent(in) :: c_outfilename
+CHARACTER(kind=C_CHAR) :: c_outfilename(*), c_datfilename(*)
+character(len=:), allocatable :: outfilename, datfilename
+integer i, nchars
+integer outfile
+
+i = 1
+do
+   if (c_outfilename(i) == c_null_char) exit
+   i = i + 1
+end do
+nchars = i - 1  ! Exclude null character from Fortran string
+!allocate(character(len=nchars) :: outfilename)
+outfilename = (repeat(' ', nchars))
+outfilename = transfer(c_outfilename(1:nchars), outfilename)
+i = 1
+do
+   if (c_datfilename(i) == c_null_char) exit
+   i = i + 1
+end do
+nchars = i - 1  ! Exclude null character from Fortran string
+!allocate(character(len=nchars) :: datfilename)
+datfilename = (repeat(' ', nchars))
+datfilename = transfer(c_datfilename(1:nchars), datfilename)
+
+!
+! Added file IO (ACS 03/16)
+!
+infile = 51
+outfile = 52
+open (unit=infile, file=datfilename, status='old', &
+    iostat=open_status, action='read', position='rewind')
+if ( open_status /= 0 ) then
+    write(outfile, *) 'Could not open GDMA input for reading.', &
+    'unit = ', infile
+    stop
+endif
+if(outfilename=='stdout') then
+  outfile=std_out
+else
+  open (unit=outfile, file=outfilename, status='old', &
+      iostat=open_status, action='write', position='append')
+  if ( open_status /= 0 ) then
+      write(outfile, *) 'Could not open psi4 output for writing.', &
+      'unit = ', infile
+      stop
+  endif
+endif
+!!!
+write(outfile, "(15x,a/15x,a//15x,a//15x,5a/14x,4a)")                  &
     "                      G D M A",                                   &
     "                  by Anthony Stone",                              &
     "Distributed Multipoles from Gaussian wavefunctions",              &
     "              version ", gdma_version, " (", commit, ")",         &
     "Compiled with ", compiler, " on ", compiled
 call time_and_date(datestring)
-print "(/2A)", "Starting at ", datestring
+write(outfile, "(/2A)") "Starting at ", datestring
 
 call start_timer
 
@@ -63,7 +122,7 @@ nat=0
 fchk=.false.
 first=.true.
 do
-  call read_line(eof)
+  call read_line(eof, infile)
   if (eof) exit
   call readu(key)
   select case(key)
@@ -99,7 +158,7 @@ do
     Qfactor=1d0
   case("COMMENT","TITLE")
     call reada(buffer)
-    print "(/a/)", trim(buffer)
+    write(outfile, "(/a/)") trim(buffer)
   case("DENSITY")
     if (fchk) call die                                         &
         ("Specify density to use before reading data file",.true.)
@@ -125,17 +184,17 @@ do
       end select
     end do
     ir=9
-    call get_data(whichd,ok)
+    call get_data(whichd,ok,outfile)
     close(9)
     ir=5
     fchk=.true.
   case ("HERE")
-    call get_data(whichd,ok)
+    call get_data(whichd,ok,outfile)
     fchk=.true.
   case("NAMES")
     if (.not. fchk) call die                                   &
         ("Read data file before specifying atom names",.false.)
-    call read_line(eof)
+    call read_line(eof, infile)
     do i=1,nat
       call geta(name(i))
     end do
@@ -156,9 +215,9 @@ do
       deallocate(densty)
       first=.false.
     endif
-    print "(//2A/)", "Using "//trim(whichd)//" density matrix",  &
+    write(outfile, "(//2A/)") "Using "//trim(whichd)//" density matrix",  &
         " from file "//trim(file)
-    call dma_main(dtri,kp)
+    call dma_main(dtri,kp,infile,outfile)
     call timer
   case("RESET")
     nat=0
@@ -175,17 +234,22 @@ do
 end do
 
 call time_and_date(datestring)
-print "(/2A)", "Finished at ", datestring
+write(outfile, "(/2A)") "Finished at ", datestring
+close(outfile)
+close(infile)
+end subroutine run_gdma
 
-CONTAINS
+
+!CONTAINS
 
 !-----------------------------------------------------------------------
 
-SUBROUTINE get_data(whichd,ok)
+SUBROUTINE get_data(whichd,ok,outfile)
 
 IMPLICIT NONE
 
 CHARACTER(LEN=*), INTENT(IN) :: whichd
+INTEGER, INTENT(IN) :: outfile
 LOGICAL, INTENT(OUT) :: ok
 
 INTEGER :: atom, i, j, k, n, nn, aok
@@ -220,6 +284,10 @@ CHARACTER(LEN=5) :: label(-5:5)=(/"h(s)","g(s)","f(s)","d(s)","sp  ",  &
 !  Conversion from normalised spherical form to normalised Cartesian
 !  Schlegel & Frisch, IJQC (1995) 54, 83-87.
 rt3v2=rt3/2d0
+!  d functions
+!   1   2   3   4   5   6
+!   xx  yy  zz  xy  xz  yz
+!  200 020 002 110 101 011
 td(1,:)=(/-0.5d0, -0.5d0, 1d0, 0d0, 0d0, 0d0/)
 td(2,:)=(/0d0,    0d0,    0d0, 0d0, 1d0, 0d0/)
 td(3,:)=(/0d0,    0d0,    0d0, 0d0, 0d0, 1d0/)
@@ -229,6 +297,7 @@ td(5,:)=(/0d0,    0d0,    0d0, 1d0, 0d0, 0d0/)
 !  f functions
 !   1   2   3   4   5   6   7   8   9   10
 !  xxx yyy zzz xxy xxz xyy yyz xzz yzz xyz
+!  300 030 003 210 201 120 021 102 012 111
 tf(:,:)=0d0
 ! 30
 tf(1,3)=1d0; tf(1,5)=-1.5d0/sqrt(5d0); tf(1,7)=-1.5d0/sqrt(5d0)
@@ -248,6 +317,8 @@ tf(7,2)=-sqrt(10d0)/4d0; tf(7,4)=0.75d0*sqrt(2d0)
 !  g functions
 !   1    2    3    4    5    6    7    8    9   10   11   12   13   14   15
 ! xxxx yyyy zzzz xxxy xxxz xyyy yyyz xzzz yzzz xxyy xxzz yyzz xxyz xyyz xyzz
+!  400  040  004  310  301  130  031  103  013  220  202  022  211  121  112
+!     12   23  D     23 23,12  13  23,12 12  D      23  12
 tg=0d0
 !  40
 tg(1,v400)=0.375d0; tg(1,v040)=0.375d0; tg(1,v004)=1d0
@@ -329,8 +400,8 @@ call stream(ir)
 read (ir,"(A80/A80)") title(1), title(2)
 density_header="Total "//trim(whichd)//" Density"
 if (verbose) then
-  print "(a/a/a/)", "Gaussian header:", trim(title(1)), trim(title(2))
-  print "(3a)", 'Looking for "', trim(density_header), '"'
+  write(outfile, "(a/a/a/)") "Gaussian header:", trim(title(1)), trim(title(2))
+  write(outfile, "(3a)") 'Looking for "', trim(density_header), '"'
 end if
 do
   read (ir,"(A)",iostat=k) buffer
@@ -338,7 +409,7 @@ do
     call stream(5)
     exit
   end if
-  if (debug(0)) print "(a)", buffer
+  if (debug(0)) write(outfile, "(a)") buffer
   text=buffer(1:40)
   type=buffer(44:44)
   ww=buffer(48:49)
@@ -347,7 +418,7 @@ do
   else
     nn=0
   endif
-  if (debug(0)) print "(a)", text
+  if (debug(0)) write(outfile, "(a)") text
   select case(text)
   case("")
     cycle
@@ -356,7 +427,7 @@ do
     exit
   case("Number of atoms")
     read(buffer,"(55X,I6)") nat
-    if (verbose) print "(i0,a)", nat, " atoms"
+    if (verbose) write(outfile, "(i0,a)") nat, " atoms"
     if (allocated(zan)) deallocate(zan,c)
     allocate(zan(nat),c(3,nat),stat=aok)
     if (aok>0) call die("Can't allocate atom arrays")
@@ -367,18 +438,18 @@ do
     if (aok>0) call die("Can't allocate site-name array")
   case("Charge")
     read(buffer,"(55X,I6)") ich
-    if (verbose) print "(a,i0)", "Charge ", ich
+    if (verbose) write(outfile, "(a,i0)") "Charge ", ich
   case("Multiplicity")
     read(buffer,"(55X,I6)") mul
-    if (verbose) print "(a,i0)", "Multiplicity ", mul
+    if (verbose) write(outfile, "(a,i0)") "Multiplicity ", mul
   case("Number of basis functions")
     read(buffer,"(55X,I6)") ncoorb
     !  This number may be increased following conversion from
     !  spherical to cartesian
-    if (verbose) print "(i0,a)", ncoorb, " basis functions"
+    if (verbose) write(outfile, "(i0,a)") ncoorb, " basis functions"
   case("Number of contracted shells")
     read(buffer,"(55X,I6)") nshell
-    if (verbose) print "(i0,a)", nshell, " shells"
+    if (verbose) write(outfile, "(i0,a)") nshell, " shells"
     if (allocated(kstart))                                             &
         deallocate(kstart,katom,ktype,kng,kloc,kmin,kmax,shell_type)
     allocate (kstart(nshell), katom(nshell+1), ktype(nshell),          &
@@ -388,17 +459,17 @@ do
     shell_type=0
   case("Highest angular momentum")
     read(buffer,"(55X,I6)") maxl
-    if (verbose) print "(a,i0)", "Highest angular momentum ", maxl
+    if (verbose) write(outfile, "(a,i0)") "Highest angular momentum ", maxl
     if (maxl > 5) call die                                 &
         ("Sorry -- GDMA can only handle s, p, d, f, g and h basis functions",.false.)
   case("Largest degree of contraction")
     read(buffer,"(55X,I6)") cmax
-    if (verbose) print "(a,i0)", "Largest contraction depth ", cmax
+    if (verbose) write(outfile, "(a,i0)") "Largest contraction depth ", cmax
     ! if (cmax .gt. 16) call die                                &
     !     ("Sorry -- maximum contraction depth is 16",.false.)
   case("Number of primitive shells")
     read(buffer,"(55X,I6)") nprim
-    if (verbose) print "(i0,a)", nprim, " primitive shells"
+    if (verbose) write(outfile, "(i0,a)") nprim, " primitive shells"
     if (allocated(ex)) deallocate(ex,cs,cp)
     allocate(ex(nprim), cs(nprim), cp(nprim), stat=aok)
     if (aok>0) then
@@ -411,23 +482,23 @@ do
       call geti(k)
       name(i)=element(k)
     end do
-    if (verbose) print "(a,20a3/(17x,20a3))",                    &
+    if (verbose) write(outfile, "(a,20a3/(17x,20a3))")                    &
         "Atoms:            ", name(1:nat)
   case("Nuclear charges")
     call read_line(eof)
     do i=1,nat
       call getf(zan(i))
     end do
-    if (verbose) print "(a,20i3/(16x,20i3))",                    &
+    if (verbose) write(outfile, "(a,20i3/(16x,20i3))")                    &
         "Nuclear charges:", nint(zan(1:nat))
   case("Current cartesian coordinates")
     call read_line(eof)
-    if (verbose) print "(a)", "Atom  Z   Position (a.u.)"
+    if (verbose) write(outfile, "(a)") "Atom  Z   Position (a.u.)"
     do i=1,nat
       do j=1,3
         call getf(c(j,i),rfact)
       end do
-      if (verbose) print "(a3,i4,3f10.5)", name(i), nint(zan(i)), c(:,i)
+      if (verbose) write(outfile, "(a3,i4,3f10.5)") name(i), nint(zan(i)), c(:,i)
     end do
   case("Shell types")
     call read_line(eof)
@@ -467,9 +538,9 @@ do
         num=num+13; n=n+28
       end select
     end do
-    if (verbose) print "(i0,a)", num, " basis functions"
+    if (verbose) write(outfile, "(i0,a)") num, " basis functions"
     if ((verbose) .and. n>num)                           &
-        print "(a,i0,a)", "(", n, " after conversion to cartesian)"
+        write(outfile, "(a,i0,a)") "(", n, " after conversion to cartesian)"
     maxbfn = n
     if (allocated(iax)) deallocate(iax)
     allocate(iax(n+1), stat=aok)
@@ -486,8 +557,8 @@ do
       kng(i)=j
     end do
     if (verbose) then
-      print "(a,20i3/(19x,20i3))", "Contraction depths:", kng(1:nshell)
-      print "(a,i0)", "Total number of primitives required: ", k-1
+      write(outfile, "(a,20i3/(19x,20i3))") "Contraction depths:", kng(1:nshell)
+      write(outfile, "(a,i0)") "Total number of primitives required: ", k-1
     end if
     if (k .ne. nprim+1) call die                              &
         ("Shell contractions do not match number of primitives",.false.)
@@ -496,6 +567,9 @@ do
     do i=1,nshell
       call geti(katom(i))
     end do
+    if (verbose) then
+      write(outfile, "(a,120i3)") "shell to atom", katom(1:nshell)
+    endif
   case("Primitive exponents")
     call read_line(eof)
     do i=1,nprim
@@ -534,7 +608,7 @@ do
     end do
 !   case("Alpha MO coefficients","Beta MO coefficients")
 !     if (debug(2)) then
-!       print "(/a)", text
+!       write(outfile, "(/a)") text
 !       allocate(temp(ncoorb,ncoorb))
 !       do i=1,ncoorb
 !         do j=1,ncoorb
@@ -578,19 +652,19 @@ if (verbose) then
   do i=1,nshell
     if (katom(i) .ne. atom) then
       atom=katom(i)
-      print "(a)", name(atom)
+      write(outfile, "(a)") name(atom)
     end if
-    print "(a,i0,3x,a)", "Shell ", i, label(shell_type(i))
+    write(outfile, "(a,i0,3x,a)") "Shell ", i, label(shell_type(i))
     do j=kstart(i),kstart(i)+kng(i)-1
       select case (shell_type(i))
       case (-1)
-        print "(i10,f16.8, 2f14.8)", j, ex(j), cs(j), cp(j)
+        write(outfile, "(i10,f16.8, 2f14.8)") j, ex(j), cs(j), cp(j)
       case(0)
-        print "(i10,f16.8, 2f14.8)", j, ex(j), cs(j)
+        write(outfile, "(i10,f16.8, 2f14.8)") j, ex(j), cs(j)
       case(1)
-        print "(i10,f16.8, 2f14.8)", j, ex(j), cp(j)
+        write(outfile, "(i10,f16.8, 2f14.8)") j, ex(j), cp(j)
       case default
-        print "(i10,f16.8, 2f14.8)", j, ex(j), cs(j)
+        write(outfile, "(i10,f16.8, 2f14.8)") j, ex(j), cs(j)
       end select
     end do
   end do
@@ -672,7 +746,7 @@ do i=1,nshell
     kmin(i)=21
     kmax(i)=35
     ktype(i)=5
-    ! print "(a,i0,a,i0)", "num = ", num, "  k = ", k
+    ! write(outfile, "(a,i0,a,i0)") "num = ", num, "  k = ", k
     if (shell_type(i) .lt. 0) then ! Spherical g shell
       temp(1:num,1:k)=densty(1:num,1:k)
       temp(1:num,k+1:k+15)=matmul(densty(1:num,k+1:k+9),tg)
@@ -686,7 +760,7 @@ do i=1,nshell
     kmin(i)=36
     kmax(i)=56
     ktype(i)=6
-    ! print "(a,i0,a,i0)", "num = ", num, "  k = ", k
+    ! write(outfile, "(a,i0,a,i0)") "num = ", num, "  k = ", k
     if (shell_type(i) .lt. 0) then ! Spherical h shell
       temp(1:num,1:k)=densty(1:num,1:k)
       temp(1:num,k+1:k+21)=matmul(densty(1:num,k+1:k+11),th)
@@ -747,4 +821,169 @@ end do
 
 END SUBROUTINE matwrtt
 
-END PROGRAM gdma
+END module libgdma
+
+program gdma
+
+!call run_gdma("stdout", "qwer")
+
+USE input
+USE version
+USE dma
+USE atom_grids, ONLY: debug_grid => debug
+USE timing, ONLY: start_timer, timer, time_and_date
+use libgdma
+!IMPLICIT NONE
+!
+!INTEGER, PARAMETER :: dp=kind(1d0)
+!
+!CHARACTER(LEN=100) :: file
+!CHARACTER(LEN=80) :: buffer
+!CHARACTER(LEN=20) :: key
+!CHARACTER(LEN=8) :: whichd="SCF"
+!CHARACTER(LEN=24) :: datestring
+!
+!!  Maximum number of sites is number of atoms + nextra
+!INTEGER :: nextra=16
+!INTEGER :: ncoorb, maxl, cmax, nprim, nx, num, ich, mul
+!INTEGER, ALLOCATABLE :: shell_type(:)
+!INTEGER :: i, j, k, kp=0
+!LOGICAL :: eof, fchk, first, ok=.false.
+!
+!REAL(dp), ALLOCATABLE :: densty(:,:), dtri(:)
+!INTEGER :: ir=5 ! Input stream
+!
+!LOGICAL :: verbose=.false., debug(0:2)=.false.
+
+
+print "(15x,a/15x,a//15x,a//15x,5a/14x,4a)",                           &
+    "                      G D M A",                                   &
+    "                  by Anthony Stone",                              &
+    "Distributed Multipoles from Gaussian wavefunctions",              &
+    "              version ", gdma_version, " (", commit, ")",         &
+    "Compiled with ", compiler, " on ", compiled
+call time_and_date(datestring)
+print "(/2A)", "Starting at ", datestring
+
+call start_timer
+
+punchfile="dma.punch"
+nat=0
+fchk=.false.
+first=.true.
+do
+  call read_line(eof)
+  if (eof) exit
+  call readu(key)
+  select case(key)
+  case("","NOTE","!")
+    cycle
+  case("VERBOSE")
+    verbose=.true.
+  case("QUIET")
+    debug=.false.
+    verbose=.false.
+  case("DEBUG")
+    debug(0)=.true.
+    do while (item<nitems)
+      call readi(k)
+      if (k>0) then
+        debug(k)=.true.
+      else
+        debug(-k)=.false.
+      end if
+    end do
+    debug_grid=.true.
+    verbose=.true.
+  case("ANGSTROM")
+    rfact=bohr
+  case("BOHR")
+    rfact=1d0
+  case("SI")
+    Qfactor(0)=echarge
+    do k=1,20
+      Qfactor(k)=Qfactor(k-1)*bohr
+    end do
+  case("AU")
+    Qfactor=1d0
+  case("COMMENT","TITLE")
+    call reada(buffer)
+    print "(/a/)", trim(buffer)
+  case("DENSITY")
+    if (fchk) call die                                         &
+        ("Specify density to use before reading data file",.true.)
+    call readu(whichd)
+  case("FILE","READ")
+    nat=0
+    fchk=.false.
+    ok=.false.
+    first=.true.
+    if (allocated(dtri)) deallocate(dtri)
+    do while (item<nitems)
+      call readu(key)
+      select case(key)
+      case("DENSITY")
+        call readu(whichd)
+      case default
+        call reread(-1)
+        call reada(file)
+        open(unit=9,file=file,status="old",iostat=k)
+        if (k .ne. 0) then
+          call die("Can't open file "//file,.true.)
+        endif
+      end select
+    end do
+    ir=9
+    call get_data(whichd,ok,5)
+    close(9)
+    ir=5
+    fchk=.true.
+  case ("HERE")
+    call get_data(whichd,ok,5)
+    fchk=.true.
+  case("NAMES")
+    if (.not. fchk) call die                                   &
+        ("Read data file before specifying atom names",.false.)
+    call read_line(eof)
+    do i=1,nat
+      call geta(name(i))
+    end do
+  case("GO","START","MULTIPOLES")
+    if (.not. ok) then
+      call die (trim(whichd)//" density not found",.false.)
+    endif
+    if (first) then
+      ! convert density matrix to triangular form
+      allocate(dtri(nx))
+      k=0
+      do i=1,num
+        do j=1,i
+          k=k+1
+          dtri(k)=densty(i,j)
+        end do
+      end do
+      deallocate(densty)
+      first=.false.
+    endif
+    print "(//2A/)", "Using "//trim(whichd)//" density matrix",  &
+        " from file "//trim(file)
+    call dma_main(dtri,kp,5,6)
+    call timer
+  case("RESET")
+    nat=0
+    fchk=.false.
+    ok=.false.
+    first=.true.
+    deallocate(dtri)
+    whichd="SCF"
+  case("FINISH")
+    exit
+  case default
+    call die("Keyword "//trim(key)//" not recognized",.true.)
+  end select
+end do
+
+call time_and_date(datestring)
+print "(/2A)", "Finished at ", datestring
+
+END program gdma
